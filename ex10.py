58eb67bf9a4beceecb8ec123d596a5de8265eb7f
AbdielMelendez/ <abdielhmelendez@gmail.com>
2016-01-04 15:46:20 -0800

    ex10.py

---
+++ ex10.py
@@ -0,0 +1,12 @@
+#Sum of primes up below two million
+
+def sumPrimes(n):
+    sum, sieve = 0, [True] * n
+    for p in range(2, n):
+        if sieve[p]:
+            sum += p
+            for i in range(p*p, n, p):
+                sieve[i] = False
+    return sum
+
+print sumPrimes(2000000)
